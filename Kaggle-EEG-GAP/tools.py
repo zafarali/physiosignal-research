49a15d146cc623b0dfeb0762ee23cec153b024c7
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-07-24 01:02:47 -0400

    Add pre-processing code, need to run on a faster computer

--- Kaggle-EEG-GAP/tools.py
+++ Kaggle-EEG-GAP/tools.py
@@ -6,11 +6,11 @@ from glob import glob
 import os
 
 def prepare_data_train(subject_id, series_id):
-	"""
-		read and prepare training data frame
-		@params: 
-			subject_id: subject number
-			series_id: the series number
+    """
+        read and prepare training data frame
+        @params: 
+            subject_id: subject number
+            series_id: the series number
         @return:
             data, labels
     """
@@ -18,7 +18,7 @@ def prepare_data_train(subject_id, series_id):
     data = pd.read_csv('./data/train/subj' + str(subject_id) + '_series' + str(series_id) + '_data.csv')
     labels = pd.read_csv('./data/train/subj' + str(subject_id) + '_series' + str(series_id) + '_events.csv')
 
-    # drop the id column
+    # drop the id column since the rows are already algined.
     data = data.drop(['id'], axis=1)
     labels = labels.drop(['id'], axis=1)
 
@@ -29,9 +29,9 @@ def prepare_data_test(subject_id, series_id):
     return data
 
 
-scaler = StandardScaler()
+scaler = StandardScaler() # removes the mean and scales the variance to 1 unit.
 
-def pre_process_train(X):
+def data_preprocess_train(X):
     X_prep = scaler.fit_transform(X)
 
     # preprocessing here
